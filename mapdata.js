bca14d58a68c89d6c6f334238392b3b010434900
Felipe Sotello <fsotello@gmail.com>
2021-03-01 08:21:54 -0300

    Add files via upload

---
+++ mapdata.js
@@ -0,0 +1,330 @@
+var simplemaps_usmap_mapdata={
+  main_settings: {
+   //General settings
+    width: "960", //'700' or 'responsive'
+    background_color: "white",
+    background_transparent: "yes",
+    
+    //Label defaults
+    label_color: "#FC443C",
+    hide_labels: "no",
+    border_color: "white",
+    state_description: "",
+    state_color: "#F7F4D3",
+    state_hover_color: "#FE764C",
+    state_url: "",
+    all_states_inactive: "no",
+    
+    //Location defaults
+    location_description: "Field Office",
+    location_color: "#ff46a2",
+    location_pulse: "yes",
+    location_pulse_size: "3",
+    location_opacity: ".8",
+    location_url: "http://simplemaps.com",
+    all_states_zoomable: "yes",
+    location_size: "40",
+    location_type: "marker",
+    all_locations_inactive: "no",
+    url_new_tab: "yes",
+    auto_load: "yes",
+   
+    //Zoom settings
+    zoom: "yes",
+    initial_zoom: "",
+    initial_zoom_solo: "",
+    
+    //Advanced settings
+    div: "map",
+    state_image_url: "",
+    state_image_position: "",
+    location_image_url: "",
+    label_hover_color: "white",
+    label_size: "20px",
+    label_font: "DM Sans",
+    popups: "detect",
+    border_hover_color: "white",
+    border_size: "1",
+    border_hover_size: "1"
+  },
+  state_specific: {
+    HI: {
+      name: "Hawaii",
+      hide: "no",
+      inactive: "no"
+    },
+    AK: {
+      name: "Alaska",
+      hide: "no",
+      inactive: "no"
+    },
+    FL: {
+      name: "Florida",
+      inactive: "no",
+      hide: "no"
+    },
+    NH: {
+      name: "New Hampshire",
+      hide: "no",
+      inactive: "no"
+    },
+    VT: {
+      name: "Vermont",
+      hide: "no",
+      inactive: "no"
+    },
+    ME: {
+      name: "Maine",
+      hide: "no",
+      inactive: "no"
+    },
+    RI: {
+      name: "Rhode Island",
+      hide: "no",
+      inactive: "no"
+    },
+    NY: {
+      name: "New York",
+      hide: "no",
+      inactive: "no"
+    },
+    PA: {
+      name: "Pennsylvania",
+      hide: "no",
+      inactive: "no"
+    },
+    NJ: {
+      name: "New Jersey",
+      hide: "no",
+      inactive: "no"
+    },
+    DE: {
+      name: "Delaware",
+      hide: "no",
+      inactive: "no"
+    },
+    MD: {
+      name: "Maryland",
+      hide: "no",
+      inactive: "no"
+    },
+    VA: {
+      name: "Virginia",
+      hide: "no",
+      inactive: "no"
+    },
+    WV: {
+      name: "West Virginia",
+      hide: "no",
+      inactive: "no"
+    },
+    OH: {
+      name: "Ohio",
+      hide: "no",
+      inactive: "no"
+    },
+    IN: {
+      name: "Indiana",
+      hide: "no",
+      inactive: "no"
+    },
+    IL: {
+      name: "Illinois",
+      hide: "no",
+      inactive: "no"
+    },
+    CT: {
+      name: "Connecticut",
+      hide: "no",
+      inactive: "no"
+    },
+    WI: {
+      name: "Wisconsin",
+      hide: "no",
+      inactive: "no"
+    },
+    NC: {
+      name: "North Carolina",
+      hide: "no",
+      inactive: "no"
+    },
+    DC: {
+      name: "District of Columbia",
+      hide: "no",
+      inactive: "no"
+    },
+    MA: {
+      name: "Massachusetts",
+      hide: "no",
+      inactive: "no"
+    },
+    TN: {
+      name: "Tennessee",
+      hide: "no",
+      inactive: "no"
+    },
+    AR: {
+      name: "Arkansas",
+      hide: "no",
+      inactive: "no"
+    },
+    MO: {
+      name: "Missouri",
+      hide: "no",
+      inactive: "no"
+    },
+    GA: {
+      name: "Georgia",
+      hide: "no",
+      inactive: "no"
+    },
+    SC: {
+      name: "South Carolina",
+      hide: "no",
+      inactive: "no"
+    },
+    KY: {
+      name: "Kentucky",
+      hide: "no",
+      inactive: "no"
+    },
+    AL: {
+      name: "Alabama",
+      hide: "no",
+      inactive: "no"
+    },
+    LA: {
+      name: "Louisiana",
+      hide: "no",
+      inactive: "no"
+    },
+    MS: {
+      name: "Mississippi",
+      hide: "no",
+      inactive: "no"
+    },
+    IA: {
+      name: "Iowa",
+      hide: "no",
+      inactive: "no"
+    },
+    MN: {
+      name: "Minnesota",
+      hide: "no",
+      inactive: "no"
+    },
+    OK: {
+      name: "Oklahoma",
+      hide: "no",
+      inactive: "no"
+    },
+    TX: {
+      name: "Texas",
+      hide: "no",
+      inactive: "no"
+    },
+    NM: {
+      name: "New Mexico",
+      hide: "no",
+      inactive: "no"
+    },
+    KS: {
+      name: "Kansas",
+      hide: "no",
+      inactive: "no"
+    },
+    NE: {
+      name: "Nebraska",
+      hide: "no",
+      inactive: "no"
+    },
+    SD: {
+      name: "South Dakota",
+      hide: "no",
+      inactive: "no"
+    },
+    ND: {
+      name: "North Dakota",
+      hide: "no",
+      inactive: "no"
+    },
+    WY: {
+      name: "Wyoming",
+      hide: "no",
+      inactive: "no"
+    },
+    MT: {
+      name: "Montana",
+      hide: "no",
+      inactive: "no"
+    },
+    CO: {
+      name: "Colorado",
+      hide: "no",
+      inactive: "no"
+    },
+    UT: {
+      name: "Utah",
+      hide: "no",
+      inactive: "no"
+    },
+    AZ: {
+      name: "Arizona",
+      hide: "no",
+      inactive: "no"
+    },
+    NV: {
+      name: "Nevada",
+      hide: "no",
+      inactive: "no"
+    },
+    OR: {
+      name: "Oregon",
+      hide: "no",
+      inactive: "no"
+    },
+    WA: {
+      name: "Washington",
+      hide: "no",
+      inactive: "no"
+    },
+    CA: {
+      name: "California",
+      hide: "no",
+      inactive: "no"
+    },
+    MI: {
+      name: "Michigan",
+      hide: "no",
+      inactive: "no"
+    },
+    ID: {
+      name: "Idaho",
+      hide: "no",
+      inactive: "no"
+    },
+    GU: {
+      name: "Guam",
+      hide: "yes",
+      inactive: "no"
+    },
+    VI: {
+      name: "Virgin Islands",
+      hide: "yes",
+      inactive: "no"
+    },
+    PR: {
+      name: "Puerto Rico",
+      hide: "yes",
+      inactive: "no"
+    },
+    MP: {
+      name: "Northern Mariana Islands",
+      hide: "yes",
+      inactive: "no"
+    }
+  },
+  locations: {},
+  regions: {},
+  labels: {}
+};
