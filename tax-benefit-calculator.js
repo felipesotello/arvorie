c5b599cff9363d0a19046bacdd32076778bbeacf
Felipe Sotello <fsotello@gmail.com>
2021-06-10 22:53:00 -0300

    Update tax-benefit-calculator.js

--- tax-benefit-calculator.js
+++ tax-benefit-calculator.js
@@ -1,15 +1,33 @@
-var slider1 = document.getElementById("pppm-range");
-var output1 = document.getElementById("pppm");
-output1.innerHTML = slider1.value;
-
-slider1.oninput = function() {
-	output1.innerHTML = this.value;
+function soma() {
+	var a = parseInt(document.getElementById('NY').value, 10);
+	var b = parseInt(document.getElementById('SC').value, 10);
+	var c = parseInt(document.getElementById('MS').value, 10);
+	var d = parseInt(document.getElementById('NM').value, 10);
+	var e = parseInt(document.getElementById('KS').value, 10);
+	var f = parseInt(document.getElementById('RI').value, 10);
+	var g = parseInt(document.getElementById('OS').value, 10);
+	
+	document.getElementById('US').value = a + b + c + d + e + f + g;
 }
 
-var slider2 = document.getElementById("sswd-range");
-var output2 = document.getElementById("sswd");
-output2.innerHTML = slider2.value;
+// pppm
 
-slider2.oninput = function() {
-	output2.innerHTML = this.value;
+var pppmInput = document.getElementById('pppm-range');
+var pppmOutput = document.getElementById('pppm');
+
+pppmOutput.innerHTML = pppmInput.value;
+
+pppmInput.oninput = function() {
+	pppmOutput.innerHTML = this.value;
+}
+
+// sswd
+
+var sswdInput = document.getElementById('sswd-range');
+var sswdOutput = document.getElementById('sswd');
+
+sswdOutput.innerHTML = sswdInput.value;
+
+sswdInput.oninput = function() {
+	sswdOutput.innerHTML = this.value;
 }
